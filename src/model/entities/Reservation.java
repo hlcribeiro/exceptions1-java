5729f58729df909615f617f4edd40d6ddcc4421d
Hagner Ribeiro <hlcribeiro11@gmail.com>
2021-05-20 16:42:22 -0300

    v1 - ruim

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -48,7 +48,7 @@ public class Reservation {
 
     @Override
     public String toString(){
-        return "Room"
+        return "Room "
                 + roomNumber
                 + ", check-in: "
                 + sdf.format(checkIn)
