5c6195c62a050f66872f653d9870db5338a0c314
Hagner Ribeiro <hlcribeiro11@gmail.com>
2021-05-20 16:57:34 -0300

    v2 - meio ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,18 +31,12 @@ public class Program {
             System.out.print("Check-Out date (DD/MM/AAAA): ");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if (checkIn.before(now) || checkOut.before(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null){
+                System.out.println(error);
             }
             else {
-                if (!checkOut.after(checkIn)){
-                    System.out.println("Error in reservation: check-out date must be after check-in date");
-                }
-                else {
-                    reservation.updateDates(checkIn, checkOut);
-                    System.out.println("Reservation: " + reservation);
-                }
+                System.out.println("Reservation: " + reservation);
             }
         }
         sc.close();
